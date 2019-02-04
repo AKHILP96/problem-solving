af84bb8dab8824ca0f99d73133becb0c288b4b75
AKHIL POKLE <akhilpokle96@gmail.com>
2019-02-04 21:49:07 +0530

    Add files via upload

---
+++ min_max_sum.cpp
@@ -0,0 +1,25 @@
+#include bitsstdc++.h
+
+using namespace std;
+
+int main(){
+    long long a[5];
+   int i;
+    long long sum=0;
+    for(i=0;i5;i++)
+    {
+        cina[i];
+    }
+    sort(a,a+5);
+    for(i=0;i4;i++)
+    {
+        sum+=a[i];
+    }
+    coutsum ;
+    sum=0;
+    for(i=1;i5;i++)
+    {   
+        sum+=a[i];
+    }
+    coutsum;
+}
